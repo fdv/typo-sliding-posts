6a641d02f76a335d50b97a27e133154c6826dd4e
Frédéric de Villamil <frederic@de-villamil.com>
2009-10-10 23:29:16 +0200

    Adds separate stylesheet

--- install.rb
+++ install.rb
@@ -3,6 +3,7 @@ destination = File.join(RAILS_ROOT, '/public/')
 
 puts "Copying Typo sliding posts files"
 File.copy(File.join(source, 'javascripts/carousel.js'), File.join(destination, 'javascripts/carousel.js'))
+File.copy(File.join(source, 'stylesheets/typo_sliding_posts.css'), File.join(destination, 'stylesheets/typo_sliding_posts.css'))
 File.copy(File.join(source, 'images/carousel_previous.png'), File.join(destination, 'images/carousel_previous.png'))
 File.copy(File.join(source, 'images/carousel_next.png'), File.join(destination, 'images/carousel_next.png'))
 puts "Done"
