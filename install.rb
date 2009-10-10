52fc50e11d130a1a40cb7f022b86836ae2379e74
Frédéric de Villamil <frederic@de-villamil.com>
2009-10-10 23:32:40 +0200

    Another install fix

--- install.rb
+++ install.rb
@@ -2,9 +2,9 @@ source = File.join(RAILS_ROOT, '/vendor/plugins/typo-sliding-posts/')
 destination = File.join(RAILS_ROOT, '/public/')
 
 puts "Copying Typo sliding posts files"
-File.copy(File.join(source, 'javascripts/carousel.js'), File.join(destination, 'javascripts/carousel.js'))
-File.copy(File.join(source, 'stylesheets/typo_sliding_posts.css'), File.join(destination, 'stylesheets/typo_sliding_posts.css'))
-File.copy(File.join(source, 'images/carousel_previous.png'), File.join(destination, 'images/carousel_previous.png'))
-File.copy(File.join(source, 'images/carousel_next.png'), File.join(destination, 'images/carousel_next.png'))
+FileUtils.copy(File.join(source, 'javascripts/carousel.js'), File.join(destination, 'javascripts/carousel.js'))
+FileUtils.copy(File.join(source, 'stylesheets/typo_sliding_posts.css'), File.join(destination, 'stylesheets/typo_sliding_posts.css'))
+FileUtils.copy(File.join(source, 'images/carousel_previous.png'), File.join(destination, 'images/carousel_previous.png'))
+FileUtils.copy(File.join(source, 'images/carousel_next.png'), File.join(destination, 'images/carousel_next.png'))
 puts "Done"
 
